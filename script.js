0084ec83135d840c37e518a3e9f7c98957855b78
RobertMN076 <robertnobrega14@gmail.com>
2025-07-23 21:26:56 -0300

    projeto concluído

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const menuDropdown = document.getElementById('menu-dropdown');
 menuToggle.addEventListener('click', () => {
   menuDropdown.classList.toggle('hidden');
 });
+
+
+
+const swiper = new Swiper('.meu-carrossel', {
+  // Quantidade de slides visíveis
+  slidesPerView: 'auto', 
+  
+  // Centraliza o slide ativo
+  centeredSlides: true, 
+  
+  // Espaçamento entre os slides
+  spaceBetween: 20, 
+  
+  // Ativa o loop infinito
+  loop: true, 
+
+  // Configuração da paginação (pontinhos)
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Permite clicar nos pontinhos para navegar
+  },
+
+  // Configuração das setas de navegação
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
